8fd4a6437869a5bc9e5e73747efe88677a7f13a8
gabyzola <zolag@merrimack.edu>
2025-05-04 18:30:27 -0400

    Crypto Manager class

--- src/client/CryptoManager.java
+++ src/client/CryptoManager.java
@@ -1,5 +1,40 @@
 package client;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 public class CryptoManager {
-    
+
+    // Method to generate a secret key for AES
+    public static SecretKey generateSecretKey() throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128); // AES key size (128, 192, 256 bits)
+        return keyGenerator.generateKey();
+    }
+
+    // Method to encrypt data with AES
+    public static String encrypt(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    // Method to decrypt data with AES
+    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedData);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+        return new String(decryptedBytes);
+    }
+
+    // Example usage of converting string to SecretKey (if you have a key in Base64)
+    public static SecretKey convertToSecretKey(String keyString) {
+        byte[] decodedKey = Base64.getDecoder().decode(keyString);
+        return new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+    }
 }
